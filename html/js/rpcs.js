563aeb21335824065ffd2a48861dbe8d2f1f47bf
bitcoin space <166097829+bitspacelabs@users.noreply.github.com>
2024-12-09 17:40:13 +0800

    Add files via upload

--- html/js/rpcs.js
+++ html/js/rpcs.js
@@ -1,6 +1,144 @@
-window.baseURL = 'http://54.206.113.217:8080'
+main_baseURL = 'https://api.btcspace.org'
+main_utxoBaseRpc = 'https://mempool.space/api/address/'
+main_baseMempool = 'https://mempool.space'
+
+
+test_baseURL = 'http://54.206.113.217:8080'
+// test_baseURL = 'http://127.0.0.1:3333'
+
+test_utxoBaseRpc = 'https://mempool-testnet.fractalbitcoin.io/api/address/'
+test_baseMempool = 'https://mempool-testnet.fractalbitcoin.io'
+
+
+
+function setNet() {
+    let isTest = false 
+    if(!localStorage.net) {
+        const isLocalTest =  window.location.hostname !== '127.0.0.1'
+        isTest = isLocalTest
+    }else{
+        if(localStorage.net === 'fb-test') {
+            isTest = true
+        }
+        if(localStorage.net === 'btc') {
+            isTest = false
+        }
+    }
+    window.isTest = isTest
+    window.baseURL = isTest ? test_baseURL : main_baseURL
+    window.utxoBaseRpc = isTest ? test_utxoBaseRpc : main_utxoBaseRpc
+    window.baseMempool = isTest ? test_baseMempool : main_baseMempool
+
+    netid.value = isTest ? 'fb-test' : 'btc'
+}
+setNet()
+
+// window.baseURL = 'http://192.168.110.157:3333'
 const api2 = baseURL + `/api/vm_logs?spaceid=`
-window.baseMempool = 'https://mempool.space'
+window.netChange = () => {
+    console.log(netid.value)
+    localStorage.net = netid.value
+    location.reload()
+}
+window.BTC20SPACE = `function pub_mint(){
+    if(mint_supply < supply){
+        const to = Transaction.outputs[1].address
+        const max_mint = 50000
+        const balance = balances[to]
+        if(typeof balance === 'undefined'){
+            balance = lmint
+        }else{
+            balance = balance + lmint
+        }
+        if(balance <= max_mint) {
+            balances[to] = balance
+            mint_supply = mint_supply + lmint
+            const hash = Transaction.txid + 'i' + 1
+            let curr_utxo = {}
+            if(typeof utxos[to] !== 'undefined'){
+                curr_utxo = utxos[to]
+            }
+            curr_utxo[hash] = lmint
+            utxos[to] = curr_utxo
+        }
+    }
+}
+function pub_reutxo(){
+    const from = Transaction.inputs[0].address
+    if(typeof balances[from] !== 'undefined'){
+        if(BN.isGreaterThanOrEqualTo(balances[from], 0)){
+            if(typeof utxos[from] === 'undefined'){
+                const hash = Transaction.txid + 'i' + 1
+                let curr_utxo = {}
+                const total = balances[from]
+                curr_utxo[hash] = total
+                utxos[from] = curr_utxo
+            }
+        }
+    }
+}
+// space_type default: 0; 0: user to user; 1: user to space 2: space to user 3: space to space
+function pub_transfer(num, FUI, TUI){
+    if(typeof num === 'number'){
+        if(BN.decimal(num) === true){
+            if(num > 0) {
+                const from = Transaction.inputs[0].address
+                let to = Transaction.outputs[TUI].address
+                let avb = 0
+                let inputs = Transaction.inputs
+                
+                if(space_type == 1){
+                    to = TUI
+                }
+
+                let isFromUser = space_type == 0 || space_type == 1
+                if(isFromUser){
+                    let i = 0
+                    let from_utxo = {}
+                    while(i < inputs.length){
+                        let id = inputs[i].prevTxId + 'i' + inputs[i].outputIndex
+                        if(typeof utxos[from] !== 'undefined'){
+                            from_utxo = utxos[from]
+                            if(typeof utxos[from][id] !== 'undefined'){
+                                avb = BN.plus(avb, utxos[from][id])
+                                delete from_utxo[id]
+                            }
+                        }
+                        i = i + 1
+                    }
+                    if(typeof balances[from] !== 'undefined'){
+                        if(BN.isGreaterThanOrEqualTo(balances[from], num)){
+                            if(BN.isGreaterThanOrEqualTo(avb, num)){
+                                balances[from] = BN.minus(balances[from], num)
+                                if(typeof balances[to] !== 'undefined'){
+                                    balances[to] = BN.plus(balances[to], num)
+                                }else{
+                                    balances[to] = num
+                                }
+                                    
+                                if(avb != num){
+                                    // return self
+                                    from_utxo[Transaction.txid + 'i' + FUI] = BN.minus(avb, num)
+                                }
+                                // update
+                                utxos[from] = from_utxo
+
+                                if(typeof space_type === 0){
+                                    let curr_utxo = {}
+                                    if(typeof utxos[to] !== 'undefined'){
+                                        curr_utxo = utxos[to]
+                                    }
+                                    curr_utxo[Transaction.txid + 'i' + TUI] = num
+                                    utxos[to] = curr_utxo
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}`
 const getMempoolAddressUrl = (address) => {
     return baseMempool + '/address/' + address
 }
@@ -10,12 +148,17 @@ const getMempoolTxUrl = (txid) => {
 
 const shortAddress = (address) => {
     if(address && typeof address === 'string' && address.length > 15) {
+        if(window?.outerWidth && window.outerWidth >= 1500) {
+            return address
+        }
         if(window?.outerWidth && window.outerWidth > 1500) {
             return address.substring(0, 16) + '...' + address.substring(address.length - 16, address.length)
         }
         return address.substring(0, 8) + '...' + address.substring(address.length - 8, address.length)
     }
 }
+
+window.shortAddress = shortAddress
 
 const shortHash = (address) => {
     if(window?.outerWidth && window.outerWidth > 1500) {
@@ -27,10 +170,22 @@ const shortHash = (address) => {
 }
 window.shortHash = shortHash
 
+const shortHash2 = (address) => {
+    if(address && typeof address === 'string' && address.length > 60) {
+        return address.substring(0, 10) + '...' + address.substring(address.length - 10, address.length)
+    }
+}
+window.shortHash2 = shortHash2
+
+let page = 1
+window.next_logs = (spaceId) => {
+    page ++
+    rpc_logs(spaceId)
+}
 window.rpc_logs = (spaceId) => {
     axios.get(api2 + (spaceId || ''), {
         params: {
-            page: 2
+            page,
         }
     }).then(res => {
         console.log(res.data)
@@ -105,4 +260,53 @@ window.rpc_logs = (spaceId) => {
             }
         }
     })
+}
+
+window.wallet = {
+    async getAccounts() {
+        try {
+            return await window.unisat.getAccounts();
+        } catch (error) {
+            try {
+                return await window.okxwallet.bitcoin.getAccounts();
+            } catch (error) {
+                
+            }
+        }
+    },
+    async requestAccounts() {
+        try {
+            return await window.unisat.requestAccounts();
+        } catch (error) {
+            try {
+                return await window.okxwallet.bitcoin.requestAccounts();
+            } catch (error) {
+                
+            }
+        }
+    },
+    async signPsbt(arg){
+        try {
+            return await window.unisat.signPsbt(arg);
+        } catch (error) {
+            try {
+                console.log(error)
+                return await window.okxwallet.bitcoin.signPsbt(arg);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    async pushPsbt(arg){
+        try {
+            return await window.unisat.pushPsbt(arg);
+        } catch (error) {
+            console.log(error)
+            try {
+                return await window.okxwallet.bitcoin.pushPsbt(arg);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
 }
